60345bc5d119b7d0d9b84ac2d06948beb085a78f
Jose Antonio <joseantoniojuan@facephi.com>
2026-04-07 12:46:35 +0200

    New version: 1.38.0

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FPhiSelphIDWidgetiOSLight",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "FPhiSelphIDWidgetiOSLight",
+            targets: ["FPhiSelphIDWidgetiOSLight"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:facephi-clienters/SDK-ZipZap-SPM.git", from: "1.0.0"),
+        .package(url: "git@github.com:facephi-clienters/FPhiMBWidgetiOS-framework.git", from: "6.13.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "FPhiSelphIDWidgetiOSLight",
+            dependencies: [
+                "FPhiSelphIDWidgetiOS",
+                .product(name: "FPhiZipZap", package: "SDK-ZipZap-SPM"),
+                .product(name: "FPhiMBWidgetiOS", package: "FPhiMBWidgetiOS-framework")
+            ],
+            resources: [.copy("fphi-selphid-widget-resources-selphid-1.0.zip")]),
+        .binaryTarget(name: "FPhiSelphIDWidgetiOS",
+        url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/Selphid/FPhiSelphIDWidgetiOSLight/1.38.0/FPhiSelphIDWidgetiOSLight.zip",
+        checksum: "38dbbcf88ee4b457b4bc69315c9c82feca9101737c98090f5c2ca22ee5ba1ff7")
+    ]
+)
---
+++ Sources/FPhiSelphIDWidgetiOSLight/FPhiSelphIDWidgetiOSLight.swift
@@ -0,0 +1,12 @@
+//
+//  FPhiSelphIDWidgetiOSLight.swift
+//
+//
+//  Created by Jorge Poveda on 8/3/23.
+//
+
+import Foundation
+import FPhiSelphIDWidgetiOS
+import BlinkID
+
+private class FPhiSelphIDWidgetiOSLight {}
